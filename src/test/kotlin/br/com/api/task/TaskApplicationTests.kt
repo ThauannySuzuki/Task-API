a196bafea20f34db6f158479f597251fac97fba1
ThauannySuzuki <thauannysuzuki@gmail.com>
2024-11-22 13:24:49 -0300

    initial project

---
+++ src/test/kotlin/br/com/api/task/TaskApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.api.task
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TaskApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
